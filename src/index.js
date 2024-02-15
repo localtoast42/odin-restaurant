44fe68ea27d45571b29c92ef528198d6e7b262b0
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-02-15 16:08:44 -0500

    Update nav item styling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import { createHome } from "./home";
 
+const nav = document.querySelector('nav');
+const contentDiv = document.querySelector('#content');
+
+const homeNavItem = document.querySelector('#home');
+const menuNavItem = document.querySelector('#menu');
+const contactNavItem = document.querySelector('#contact');
+
+
+
 createHome();
