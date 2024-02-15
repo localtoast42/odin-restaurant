81a865df06e6f88ad45235d69185ee5822973ceb
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-02-15 15:42:50 -0500

    Add home page .js file

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";!function(){const e=document.querySelector("#content"),t=document.createElement("h1");t.textContent="This is a really good restaurant.";const n=document.createElement("p");n.textContent="No, really, this restaurant is really, really good.",e.appendChild(t),e.appendChild(n)}()})();
